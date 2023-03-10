bc74d7434ba5b85f5906e1bfee3a774bd403c42b
Harsh Vasisht <harryvasisht@gmail.com>
2023-03-11 01:46:30 +0530

    modified:   coffee_recommendor_update.ipynb 	modified:   database.json 	modified:   heartrate.json 	modified:   main.ipynb 	modified:   video2face.py

--- video2face.py
+++ video2face.py
@@ -5,7 +5,13 @@ import cv2
 import config
 import datacapture
 import heartpred as hp
+import pandas as pd
+import joblib
+from rich.console import Console
 
+console = Console()
+
+grid_search_pred=joblib.load('grid_search_pipeline.pkl')
 artifact_config_file = config.read_yaml('config.yaml')
 hrc = artifact_config_file['Artifacts_path']['haarcascade']
 oft = artifact_config_file['Artifacts_path']['output_folder_path']
@@ -86,6 +92,7 @@ def extract_data(data):
 # def predict(img_path ,file_path = file, emotion = pred_emt, gender = pred_gen, race = pred_race, age = age, demt = dmt):
 def predict(username = 'Default' ,file_path = file):
     data = datacapture.read_data('heartrate.json')
+    # print(data["Name"])
     if data['Name'] == username:
         
         pass
@@ -108,7 +115,51 @@ def predict(username = 'Default' ,file_path = file):
             datacapture.write_data(db, file_path)
 
 
+def coffee_recommendation(db = 'database.json', model = grid_search_pred):
+    data1 = datacapture.read_data(db)
+    usn = data1['User Name']
+    em = data1['Emotion'][1]
+    age =  data1['Age']
+    esti_hr =  data1['Estimated HeartRate']
 
+    ###  DEEP FACE EMOTIONS ###
+    # 'disgust' STRESSED
+    # 'fear'    STRESSED
+    # 'happy'   HAPPY
+    # 'surprise' HAPPY
+    # 'neutral'  SAD
+    # 'angry'   SAD
+    # 'sad'     SAD  
+
+    ### OUR DATA ###
+    # HAPPY SAD STRESSED
+
+    if em == 'neutral' or em == 'angry' or em == 'sad':
+        em = 'Sad'
+    
+    elif em == 'fear' or em == 'disgust':
+        em = 'Stressed'
+
+    elif em == 'happy' or em == 'surprise':
+        em = 'Happy'
+
+    else:
+        em = 'Happy'    
+     # create a new data point for a user
+    new_data = {
+    "Age": [age],
+    "Heart Rate": [esti_hr],
+    "Emotion": em
+    }
+    df=pd.DataFrame(new_data)
+    pred = model.predict(df)
+    return pred[0], usn, em
 
 if __name__ == '__main__':
     predict()
+    pCoffee, usn, em = coffee_recommendation()
+    
+    console.print("============================================================", style="blink bold red underline on Green")
+    console.print(f"Hello {usn}, your coffee recommendation is {pCoffee} based on your current emotion, {em}", style="blink bold red underline on Green")
+    console.print("============================================================", style="blink bold red underline on Green")
+
